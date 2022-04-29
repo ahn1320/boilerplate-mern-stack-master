c07d97e0828260cc08ff1d6d182e1eecceb95807
ahn1320 <skyhot1320@gmail.com>
2022-04-29 13:59:56 -0700

    Added Favorite movie funciton

--- client/src/components/views/MovieDetail/MovieDetail.js
+++ client/src/components/views/MovieDetail/MovieDetail.js
@@ -3,6 +3,7 @@ import { API_URL, API_KEY, IMAGE_BASE_URL } from "../../Config";
 import MainImage from "../LandingPage/Sections/MainImage";
 import MovieInfo from "./Sections/MovieInfo";
 import GridCards from "../commons/GridCards";
+import Favorite from "./Sections/Favorite";
 import { Row } from "antd";
 
 function MovieDetail(props) {
@@ -26,7 +27,7 @@ function MovieDetail(props) {
         fetch(endpointCrew)
             .then(response => response.json())
             .then(response => {
-                console.log('responseForCrew', response);
+                //console.log('responseForCrew', response);
                 setCasts(response.cast);
             })
 
@@ -45,6 +46,11 @@ function MovieDetail(props) {
             />
 
             <div style={{ width: '85%', margin: '1rem auto' }}>
+                <div style={{display: 'flex', justifyContent: 'flex-end'}}>
+                    <Favorite movieInfo={Movie} movieId={movieId} userFrom={localStorage.getItem('userId')} />
+                </div>
+
+
                 <MovieInfo
                     movie={Movie}
                 />
